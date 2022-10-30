c3f9bfd64af4970bd6176278fdb3f6666efca277
Samuel Camps <samuel.camps@student.ap.be>
2022-10-31 00:47:03 +0100

    css app/homepage

--- Frontend/it-case-app/src/pages/Tile/Tile.jsx
+++ Frontend/it-case-app/src/pages/Tile/Tile.jsx
@@ -3,7 +3,9 @@ import styles from './Tile.module.css';
 const Tile = () => {
     return (
         <div className={styles.TileStyle}>
+            
             <p>Hier komt een hoofdcategorie</p>
+                
            
         </div>
     );
