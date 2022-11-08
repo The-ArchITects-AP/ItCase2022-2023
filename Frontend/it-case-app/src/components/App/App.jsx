e8a1a959c3c2d586f852ec48bc7af7f627821d5f
ValerieSleurs <sleurs.valerie@gmail.com>
2022-11-08 20:17:17 +0100

    CORS fixed

--- Frontend/it-case-app/src/components/App/App.jsx
+++ Frontend/it-case-app/src/components/App/App.jsx
@@ -4,52 +4,45 @@ import Footer from "../Footer/Footer";
 import HomePage from '../../pages/HomePage/HomePage';
 import DetailPage from '../../pages/DetailPage/DetailPage'
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';  //v6 geinstalleerd =/= v5 
-//import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const App = () => {
-  // const [drupalContent, setDrupalContent] = useState();
+  const [drupalContent, setDrupalContent] = useState();
 
-  // //getDrupalContent() haalt content (inclusief iFrames) op van locale Drupal Valerie in JSON-formaat
+  //getDrupalContent() haalt content (inclusief iFrames) op van locale Drupal Valerie in JSON-formaat
 
-  // useEffect(() => {
-  //   getDrupalContent();
-  // }, []);
+  useEffect(() => {
+    getDrupalContent();
+  }, []);
 
-  // const getDrupalContent = async () => {
-  //   let response = await fetch('http://localhost:81/drupal/node/1?_format=json', {
-  //     method: 'GET',
-  //     mode: 'no-cors',
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //       'Accept': 'application/json',
-  //     }
-  //   });
-  //   let json = await response.json();
+  const getDrupalContent = async () => {
+    let response = await fetch('http://localhost:81/drupal/allcontent?_format=json');
+    let json = await response.json();
 
-  //   setDrupalContent(json);
+    setDrupalContent(json);
 
-  //   console.log(drupalContent);
-  // }
+    console.log(drupalContent);
+  }
 
   let data = [
     {
       id: 1,
       title: "HR",
       iframe: "www1",
-      img:'/images/hr.jpg'
+      img: '/images/hr.jpg'
 
     },
     {
       id: 2,
       title: "Sales",
       iframe: "www2",
-      img:'/images/sales.jpg'
+      img: '/images/sales.jpg'
     },
     {
       id: 3,
       title: "Engineering",
       iframe: "www3",
-      img:'/images/engineering.jpg'
+      img: '/images/engineering.jpg'
     }
   ]
 
@@ -64,7 +57,7 @@ const App = () => {
 
           <Route path='/detail/:id' element={<DetailPage data={data} />} />
 
-          <Route path='/' exact element={ <HomePage data={data} />} />
+          <Route path='/' exact element={<HomePage data={data} />} />
 
         </Routes>
 
