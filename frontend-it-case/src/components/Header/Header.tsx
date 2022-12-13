3c6e613a211eb359da7f5ecbc5d21a06c3ca1263
Qin Claes <qin.claes@student.ap.be>
2022-12-13 18:34:39 +0100

    adds login button

--- frontend-it-case/src/components/Header/Header.tsx
+++ frontend-it-case/src/components/Header/Header.tsx
@@ -1,13 +1,16 @@
-import styles from './Header.module.css';
+import styles from "./Header.module.css";
 
 const Header = () => {
-    return (
-        <div className={styles.headerContainer}>
-            <div className={styles.logoContainer}>
-                <a href="/"><img src="/LogoTheArchitects.png" alt="Logo The ArchITects" /></a>
-            </div>
-        </div>
-    );
-}
+  return (
+    <div className={styles.headerContainer}>
+      <div className={styles.logoContainer}>
+        <a href="/">
+          <img src="/LogoTheArchitects.png" alt="Logo The ArchITects" />
+        </a>
+        <a href="/.auth/login/aad">Login</a>
+      </div>
+    </div>
+  );
+};
 
 export default Header;
