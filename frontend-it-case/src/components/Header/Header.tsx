702472cc633fce66986dbfe9485d627b04939c4b
Qin Claes <qin.claes@student.ap.be>
2022-12-15 22:45:52 +0100

    adds getUserData function

--- frontend-it-case/src/components/Header/Header.tsx
+++ frontend-it-case/src/components/Header/Header.tsx
@@ -1,6 +1,24 @@
+import { useEffect, useState } from "react";
+import { UserData } from "../../types";
 import styles from "./Header.module.css";
 
 const Header = () => {
+  const [userData, setUserData] = useState<UserData[]>();
+
+  useEffect(() => {
+    getUserData();
+  }, []);
+
+  const getUserData = async () => {
+    let response = await fetch(
+      "https://kind-sea-00d89a703-1.westeurope.2.azurestaticapps.net/.auth/me"
+    );
+    let result = await response.json();
+    console.log(result);
+
+    setUserData(result as UserData[]);
+  };
+
   return (
     <div className={styles.headerContainer}>
       <div className={styles.logoContainer}>
