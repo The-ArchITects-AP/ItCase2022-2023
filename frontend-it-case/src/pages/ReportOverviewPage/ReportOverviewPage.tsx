c47a81286b743587c0be6cadc55012a009a8ca42
Qin Claes <qin.claes@student.ap.be>
2022-12-18 22:20:28 +0100

    checks if reports exist on reportoverviewpage

--- frontend-it-case/src/pages/ReportOverviewPage/ReportOverviewPage.tsx
+++ frontend-it-case/src/pages/ReportOverviewPage/ReportOverviewPage.tsx
@@ -19,55 +19,58 @@ const ReportOverviewPage = ({ reports, userData }: ReportProps) => {
     return <p>Loading...</p>;
   }
 
-  const checkRole =
-    userData?.clientPrincipal.userRoles.includes(
-      toShow[0].field_report_category.toLowerCase()
-    ) || userData?.clientPrincipal.userRoles.includes("admin");
+  if (reports) {
+    const checkRole =
+      userData?.clientPrincipal.userRoles.includes(
+        toShow[0].field_report_category.toLowerCase()
+      ) || userData?.clientPrincipal.userRoles.includes("admin");
 
-  if (checkRole) {
+    if (checkRole) {
+      return (
+        <div>
+          <div className={styles.back}>
+            <Link to="/">Back</Link>
+          </div>
+
+          <div className={styles.overviewPageContainer}>
+            {toShow.map((data: IFrame) => {
+              return (
+                <div className={styles.reportContainer}>
+                  <Link to={`/report/detail/${data.nid}`} key={data.nid}>
+                    <p className={styles.title}>
+                      {data.title} | {data.field_type_of_report}
+                    </p>
+                    <img
+                      src={`https://api.apiflash.com/v1/urltoimage?access_key=ad6f7c37ed5d4f3d9f42d09dcbcd4365&wait_until=page_loaded&url=${data.field_iframe}&delay=10`}
+                      alt=""
+                      width="500"
+                      height="300"
+                    />
+                    <img
+                      src={`https://drupal-thearchitects.westeurope.cloudapp.azure.com${data.field_thumbnail}`}
+                      alt={data.title}
+                      width="500"
+                      height="300"
+                    />
+                  </Link>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      );
+    }
+
     return (
       <div>
+        <div>forbidden</div>{" "}
         <div className={styles.back}>
           <Link to="/">Back</Link>
-        </div>
-
-        <div className={styles.overviewPageContainer}>
-          {toShow.map((data: IFrame) => {
-            return (
-              <div className={styles.reportContainer}>
-                <Link to={`/report/detail/${data.nid}`} key={data.nid}>
-                  <p className={styles.title}>
-                    {data.title} | {data.field_type_of_report}
-                  </p>
-                  <img
-                    src={`https://api.apiflash.com/v1/urltoimage?access_key=ad6f7c37ed5d4f3d9f42d09dcbcd4365&wait_until=page_loaded&url=${data.field_iframe}&delay=10`}
-                    alt=""
-                    width="500"
-                    height="300"
-                  />
-                  <img
-                    src={`https://drupal-thearchitects.westeurope.cloudapp.azure.com${data.field_thumbnail}`}
-                    alt={data.title}
-                    width="500"
-                    height="300"
-                  />
-                </Link>
-              </div>
-            );
-          })}
         </div>
       </div>
     );
   }
-
-  return (
-    <div>
-      <div>forbidden</div>{" "}
-      <div className={styles.back}>
-        <Link to="/">Back</Link>
-      </div>
-    </div>
-  );
+  return <div>empty</div>;
 };
 
 export default ReportOverviewPage;
