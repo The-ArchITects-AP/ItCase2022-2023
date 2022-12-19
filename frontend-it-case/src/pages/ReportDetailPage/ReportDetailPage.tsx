8815020e7f9eadc54605bc8178dbb5a6cf8ea85f
Qin Claes <qin.claes@student.ap.be>
2022-12-19 21:14:31 +0100

    adds if statement to activate checkRole (should probably be replaced by usestate later)

--- frontend-it-case/src/pages/ReportDetailPage/ReportDetailPage.tsx
+++ frontend-it-case/src/pages/ReportDetailPage/ReportDetailPage.tsx
@@ -17,44 +17,46 @@ const ReportDetailPage = ({ reports, userData }: ReportProps) => {
 
   console.log(toShow);
 
-  const checkRole =
-    userData?.clientPrincipal.userRoles.includes(
-      toShow.field_report_category.toLowerCase()
-    ) || userData?.clientPrincipal.userRoles.includes("admin");
+  if (toShow) {
+    const checkRole =
+      userData?.clientPrincipal.userRoles.includes(
+        toShow.field_report_category.toLowerCase()
+      ) || userData?.clientPrincipal.userRoles.includes("admin");
 
-  if (checkRole) {
+    if (checkRole) {
+      return (
+        <div>
+          <div className={styles.back}>
+            <Link to={`/report/${toShow.field_report_category}`}>Back</Link>
+          </div>
+
+          <div className={styles.reportContainer} key={toShow.nid}>
+            <p className={styles.title}>{toShow.title}</p>
+            <p>
+              Id: {toShow.nid} | Category: {toShow.field_report_category} | Type
+              of report: {toShow.field_type_of_report}
+            </p>
+            <iframe
+              src={toShow.field_iframe}
+              title="Report"
+              width="1500"
+              height="500"
+              allowFullScreen={true}
+            ></iframe>
+          </div>
+        </div>
+      );
+    }
+
     return (
       <div>
+        <div>forbidden</div>{" "}
         <div className={styles.back}>
           <Link to={`/report/${toShow.field_report_category}`}>Back</Link>
-        </div>
-
-        <div className={styles.reportContainer} key={toShow.nid}>
-          <p className={styles.title}>{toShow.title}</p>
-          <p>
-            Id: {toShow.nid} | Category: {toShow.field_report_category} | Type
-            of report: {toShow.field_type_of_report}
-          </p>
-          <iframe
-            src={toShow.field_iframe}
-            title="Report"
-            width="1500"
-            height="500"
-            allowFullScreen={true}
-          ></iframe>
         </div>
       </div>
     );
   }
-
-  return (
-    <div>
-      <div>forbidden</div>{" "}
-      <div className={styles.back}>
-        <Link to={`/report/${toShow.field_report_category}`}>Back</Link>
-      </div>
-    </div>
-  );
 };
 
 export default ReportDetailPage;
