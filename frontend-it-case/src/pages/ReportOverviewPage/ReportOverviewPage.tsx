81cc6997510edc12ba4e6fbe249a40c0c07c3d43
Qin Claes <qin.claes@student.ap.be>
2022-12-19 19:59:59 +0100

    changes api

--- frontend-it-case/src/pages/ReportOverviewPage/ReportOverviewPage.tsx
+++ frontend-it-case/src/pages/ReportOverviewPage/ReportOverviewPage.tsx
@@ -50,7 +50,7 @@ const ReportOverviewPage = ({ reports, userData }: ReportProps) => {
                       {data.title} | {data.field_type_of_report}
                     </p>
                     <img
-                        src={`http://api.screenshotlayer.com/api/capture?95aedceffcd1178017db59c46da59a35&url=${encodeFieldIframeUrl(data.field_iframe)}`}
+                        src={`https://screenshot.abstractapi.com/v1/?api_key=142b1ca286734bc6a187dfa5e45376e5&url=${data.field_iframe}`}
                         //src={`https://api.apiflash.com/v1/urltoimage?access_key=ad6f7c37ed5d4f3d9f42d09dcbcd4365&wait_until=page_loaded&url=${data.field_iframe}&delay=10`}
                       alt=""
                       width="500"
