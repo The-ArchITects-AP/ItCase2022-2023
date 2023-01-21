86e331c6bbf150272a897ba48d8c8521676c92ea
ValerieSleurs <sleurs.valerie@gmail.com>
2023-01-21 18:41:18 +0100

    back

--- frontend-it-case/src/pages/ReportOverviewPage/ReportOverviewPage.tsx
+++ frontend-it-case/src/pages/ReportOverviewPage/ReportOverviewPage.tsx
@@ -105,8 +105,9 @@ const ReportOverviewPage = ({ reports, userData }: ReportProps) => {
                         }
                       </div>
                       <div className={styles.text}>
-                        <p className={styles.title}>{data.title}</p>                    
-                        <p>Added on: {data.field_date.toDateString()}</p>
+                        <p className={styles.title}>{data.title}</p>
+                        <p>{data.field_type_of_report} | {data.field_report_category}</p>
+                        <p>Added: {data.field_date.toDateString()}</p>
                       </div>
                     </div>
                   </Link>
