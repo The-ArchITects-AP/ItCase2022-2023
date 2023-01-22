0e696e0cd8bed7e7a5495056520ffc8bd28251f3
ValerieSleurs <sleurs.valerie@gmail.com>
2023-01-22 15:43:43 +0100

    Tijdelijke data uitgeschakeld

--- frontend-it-case/src/pages/ReportDetailPage/ReportDetailPage.tsx
+++ frontend-it-case/src/pages/ReportDetailPage/ReportDetailPage.tsx
@@ -6,35 +6,35 @@ import ForbiddenPage from "../ForbiddenPage/ForbiddenPage";
 //Tijdelijke data voor CSS
 //Drupal data niet zichtbaar op localhost
 
-const reportsTemporary = [
-  {
-    nid: "14",
-    field_report_category: "Engineering",
-    title: "COVID",
-    field_date: "2022-03-25",
-    field_type_of_report: "Power Bi ",
-    field_thumbnail: "",
-    field_iframe: "https://app.powerbi.com/view?r=eyJrIjoiYjA1YzhhMjItMWU1ZS00YmRiLWI3MjUtZDVhN2ZlMzY4NjFlIiwidCI6IjJjOTJmZjI0LWI0MmMtNDgwZC1iNzRkLTY2ZmNlNzZiZDdkYSIsImMiOjl9"
-    },
-    {
-    nid: "12",
-    field_report_category: "Engineering",
-    title: "COVID",
-    field_date: "2022-03-25",
-    field_type_of_report: "Power Bi ",
-    field_thumbnail: "/sites/default/files/2022-12/Covid_Thumbnail_3.jpg",
-    field_iframe: "https://app.powerbi.com/view?r=eyJrIjoiYjA1YzhhMjItMWU1ZS00YmRiLWI3MjUtZDVhN2ZlMzY4NjFlIiwidCI6IjJjOTJmZjI0LWI0MmMtNDgwZC1iNzRkLTY2ZmNlNzZiZDdkYSIsImMiOjl9"
-    },
-    {
-    nid: "8",
-    field_report_category: "Engineering",
-    title: "The Rat Pack",
-    field_date: "2022-03-25",
-    field_type_of_report: "Tableau",
-    field_thumbnail: "/sites/default/files/2022-12/RatPack_Thumbnail_1.jpg",
-    field_iframe: "https://public.tableau.com/views/RunningWiththePack/RunningwiththePack?:showVizHome=no&:embed=true&:language=en-US&:display_count=n&:origin=viz_share_link&:auto_resize=yes"
-    }
-]
+// const reportsTemporary = [
+//   {
+//     nid: "14",
+//     field_report_category: "Engineering",
+//     title: "COVID",
+//     field_date: "2022-03-25",
+//     field_type_of_report: "Power Bi ",
+//     field_thumbnail: "",
+//     field_iframe: "https://app.powerbi.com/view?r=eyJrIjoiYjA1YzhhMjItMWU1ZS00YmRiLWI3MjUtZDVhN2ZlMzY4NjFlIiwidCI6IjJjOTJmZjI0LWI0MmMtNDgwZC1iNzRkLTY2ZmNlNzZiZDdkYSIsImMiOjl9"
+//     },
+//     {
+//     nid: "12",
+//     field_report_category: "Engineering",
+//     title: "COVID",
+//     field_date: "2022-03-25",
+//     field_type_of_report: "Power Bi ",
+//     field_thumbnail: "/sites/default/files/2022-12/Covid_Thumbnail_3.jpg",
+//     field_iframe: "https://app.powerbi.com/view?r=eyJrIjoiYjA1YzhhMjItMWU1ZS00YmRiLWI3MjUtZDVhN2ZlMzY4NjFlIiwidCI6IjJjOTJmZjI0LWI0MmMtNDgwZC1iNzRkLTY2ZmNlNzZiZDdkYSIsImMiOjl9"
+//     },
+//     {
+//     nid: "8",
+//     field_report_category: "Engineering",
+//     title: "The Rat Pack",
+//     field_date: "2022-03-25",
+//     field_type_of_report: "Tableau",
+//     field_thumbnail: "/sites/default/files/2022-12/RatPack_Thumbnail_1.jpg",
+//     field_iframe: "https://public.tableau.com/views/RunningWiththePack/RunningwiththePack?:showVizHome=no&:embed=true&:language=en-US&:display_count=n&:origin=viz_share_link&:auto_resize=yes"
+//     }
+// ]
 
 interface ReportProps {
   reports: IFrame[];
@@ -43,7 +43,7 @@ interface ReportProps {
 
 const ReportDetailPage = ({ reports, userData }: ReportProps) => {
   let { nid } = useParams();
-  const toShow = reportsTemporary.find((item) => item.nid === nid);
+  const toShow = reports.find((item) => item.nid === nid);
 
   if (!toShow) {
     return <p>Loading...</p>;
@@ -51,12 +51,12 @@ const ReportDetailPage = ({ reports, userData }: ReportProps) => {
 
   console.log(toShow);
 
-  // const checkRole =
-  //   userData?.clientPrincipal.userRoles.includes(
-  //     toShow.field_report_category.toLowerCase()
-  //   ) || userData?.clientPrincipal.userRoles.includes("admin");
+  const checkRole =
+    userData?.clientPrincipal.userRoles.includes(
+      toShow.field_report_category.toLowerCase()
+    ) || userData?.clientPrincipal.userRoles.includes("admin");
 
-  const checkRole = true;
+  // const checkRole = true;
 
   if (checkRole) {
     return (
